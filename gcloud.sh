6f4c3b75741549b4807c05f7f3adc4b200a12e9a
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2022-04-02 21:36:17 +0200

    Delete gcloud.sh

--- gcloud.sh
+++
@@ -1,6 +0,0 @@
-export GCP_PROJECT_ID='GCP_PROJECT_ID'
-export SERVICE_ACCOUNT='service-account@example.iam.gserviceaccount.com'
-export CLUSTER_NAME='CLUSTER_NAME'
-export ZONE='europe-north1-c'        # gcloud compute zones list
-export REGION='europe-north1'        # gcloud compute regions list
-export MACHINE_TYPE='n1-standard-8'  # gcloud compute machine-types list --zones=$ZONE
